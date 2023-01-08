3f648c6090a875897d8d3e4c566fadb15f29ba1f
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-07 20:39:44 -0700

    Button: added glasssButton variant
    glassmorphism button, with bottom border removed, gave it a glow as well. will need to animate the glow to pulse slightly

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -67,6 +67,21 @@ const _ButtonVariants = (toggleState?: boolean): _ButtonVariants => {
 	return {
 		default: {
 			default: ``,
+			glassButton: `
+				background: transparent;
+				height: 9.5rem;
+				width: 30rem;
+				border: 0.5rem solid rgba(255,255,255,0.7);
+				border-bottom: transparent;
+				border-radius: 1.5rem;
+				
+				backdrop-filter: blur(1rem);
+				box-shadow: 0rem 0rem 0.5rem 0.1rem rgba(255,255,255,0.5);
+				
+				color: rgba(255,255,255,0.8);
+				font-size: 3rem;
+				text-shadow: 0rem 0.1rem 0.5rem rgba(255,255,255,0.5);
+			`,
 		},
 		AppToggle: {
 			slider: `
