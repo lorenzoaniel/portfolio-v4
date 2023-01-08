a8af4f79897c54e1d53193268785a8a33ed56f2c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-07 20:40:20 -0700

    Home: slight styling added test button
    cleaned up sstyling in order to test a button comp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "../components/Button";
 
 const Home = () => {
-	return <_Home.Main>Toggled On Test</_Home.Main>;
+	return <_Home.Main>{<Button nameProp={"Test"} variant={"glassButton"} />}</_Home.Main>;
 };
 
 const _Home = {
 	Main: styled.section`
+		/* background-color: white; */
+		width: 100%;
+		height: 100%;
 		color: white;
 		font-size: 3rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
 	`,
 };
 
