078171400e7ed2072f6e26b25661a05239da6425
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-16 20:23:13 -0700

    Home: info in state management
    decoupled static portfolio data to redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../components/Button";
-import Carousel from "../components/Carousel";
+import Decorations from "../components/Decorations";
+import { useAppSelector } from "../store/hooks";
+import { selectHomeInfo } from "../store/slices/homePageSlice";
+import Paragraph from "../components/Paragraph";
 
 const Home = () => {
+	const porfolioInfo = useAppSelector(selectHomeInfo);
 	return (
 		<_Home.Main>
-			{
-				<Carousel>
-					{[
-						<Button nameProp={"Home"} variant={"glassButton"} />,
-						<Button nameProp={"About Me"} variant={"glassButton"} />,
-						<Button nameProp={"Works"} variant={"glassButton"} />,
-						<Button nameProp={"Contact Me"} variant={"glassButton"} />,
-					]}
-				</Carousel>
-			}
+			<Paragraph data={porfolioInfo} subComp="HomePage" />
 		</_Home.Main>
 	);
 };
 
 const _Home = {
 	Main: styled.section`
+		/* background-image: linear-gradient(to left, rgba(255, 255, 255, 0.2), rgba(230, 230, 230, 0.5)); */
 		width: 100%;
-		height: 100%;
-		color: white;
+		height: 15rem;
+		color: black;
 		font-size: 3rem;
 		display: flex;
 		flex-direction: column;
-		justify-content: flex-end;
+		justify-content: center;
+		align-items: center;
+		align-self: flex-end;
 	`,
 };
 
