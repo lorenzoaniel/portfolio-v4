cc4355d1a56d9bca693d67ab21b58b56595b075c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-16 20:23:55 -0700

    store: added homePageSlice & carouselSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { appToggleSlice } from "./slices/appToggleSlice";
+import { homePageSlice } from "./slices/homePageSlice";
+import { carouselSlice } from "./slices/carouselSlice";
 
 export const store = configureStore({
 	reducer: {
 		toggle: appToggleSlice.reducer,
+		homePageInfo: homePageSlice.reducer,
+		carouselSlice: carouselSlice.reducer,
 	},
 });
 
