34a8575c386319ce5e111c646e029e32be60fff7
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-20 00:31:01 -0700

    Home: refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "../components/Button";
-import Decorations from "../components/Decorations";
 import { useAppSelector } from "../store/hooks";
 import { selectHomeInfo } from "../store/slices/homePageSlice";
 import Paragraph from "../components/Paragraph";
@@ -10,14 +8,13 @@ const Home = () => {
 	const porfolioInfo = useAppSelector(selectHomeInfo);
 	return (
 		<_Home.Main>
-			<Paragraph data={porfolioInfo} subComp="HomePage" />
+			<Paragraph data={porfolioInfo.porfolioInfo} subComp="HomePage" />
 		</_Home.Main>
 	);
 };
 
 const _Home = {
 	Main: styled.section`
-		/* background-image: linear-gradient(to left, rgba(255, 255, 255, 0.2), rgba(230, 230, 230, 0.5)); */
 		width: 100%;
 		height: 15rem;
 		color: black;
