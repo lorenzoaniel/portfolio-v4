8370efc1515355704c839e248454e68f1466182d
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-20 20:58:46 -0700

    Store: added pages info

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { appToggleSlice } from "./slices/appToggleSlice";
 import { homePageSlice } from "./slices/homePageSlice";
 import { carouselSlice } from "./slices/carouselSlice";
+import { pagesInfoSlice } from "./slices/pagesInfoSlice";
 
 export const store = configureStore({
 	reducer: {
 		toggle: appToggleSlice.reducer,
 		homePageInfo: homePageSlice.reducer,
 		carouselSlice: carouselSlice.reducer,
+		pagesInfo: pagesInfoSlice.reducer,
 	},
 });
 
