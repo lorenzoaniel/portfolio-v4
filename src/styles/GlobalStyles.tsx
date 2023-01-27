e3ec7aa1db73f2051a8826f5183673ae233e0273
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-26 20:17:39 -0700

    Update GlobalStyles.tsx
    Added color vars used in About page comp

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,21 +1,33 @@
 import { createGlobalStyle, css } from "styled-components";
-import normalizeStyle from "./normalizeStyle";
 
 export const GlobalStyle = createGlobalStyle`
   * {
-      //
+    //COLORS
+    --About-Cyan-1: rgba(20, 20, 20, 0.5);
+    --About-Cyan-2: rgba(10, 80, 80, 0.5);
+    --About-Cyan-3: rgba(20, 150, 150, 0.5);
+    --About-Cyan-4: rgba(70, 150, 150, 0.7);
+    --About-Cyan-5: rgba(70, 255, 255, 0.5);
+    --About-Maroon-1: rgba(80, 10, 10, 1);
+    --About-Maroon-2: rgba(70, 20, 20, 1);
+    --About-Maroon-3: rgba(255, 70, 70, 0.5);
+
+    //FONTS
     --defaultFont: 'Inter', sans-serif;
     --roboticFont: 'Rajdhani', sans-serif;
 
+    /* * * */
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: var(--defaultFont)
+    font-family: var(--defaultFont);
+
+    scrollbar-width: none;
   }
 
   html {
-    height: 100vh;
-    width: 100vw;
+    height: clamp(66.7rem, 100%, 100vh);
+    width: 100%;
 
     font-size: 62.5%;
     body {
