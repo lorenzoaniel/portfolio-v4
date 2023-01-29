d46e594db93ce3e6a209819a7d718ba40d2dfc3a
lorenzoaniel <lorenzoaniel@gmail.com>
2023-01-28 20:28:06 -0700

    Create ALittleBitAboutMePage.tsx

---
+++ src/pages/About/Topics/ALittleBitAboutMePage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PageProps {
+	dataProp: Object;
+}
+
+const ALittleBitAboutMePage = (props: PageProps) => {
+	return <_Page.Main></_Page.Main>;
+};
+
+const _PageVariants = {};
+
+const _Page = {
+	Main: styled.section`
+		height: 100%;
+		width: 100%;
+	`,
+};
+
+export default ALittleBitAboutMePage;
