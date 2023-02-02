45004f93dedf080d2cb8f1fb722022cf4d78b539
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-01 22:28:43 -0700

    Heading: added AboutPage variant
    will need to put colours in store so that theme can change depending on topic

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -16,18 +16,68 @@ interface _HeadingProps {
 
 interface _HeadingVariants {
 	default: any;
+	AboutPage: any;
 }
 
 const Heading = (props: HeadingProps) => {
 	const { titleProp, variant = "default", subComp = "default" } = props;
-	return (
-		<_Heading.default variant={variant} subComp={subComp}>
-			{titleProp}
-		</_Heading.default>
-	);
+
+	const createheading = (variant: string, subComp: string) => {
+		switch (variant) {
+			case "AboutPage":
+				return (
+					<>
+						<_Heading.aboutPage.Main variant={variant} subComp={subComp}>
+							<_Heading.aboutPage.Content variant={variant} subComp={subComp}>
+								{titleProp}
+							</_Heading.aboutPage.Content>
+						</_Heading.aboutPage.Main>
+					</>
+				);
+			default:
+				return (
+					<>
+						<_Heading.default variant={variant} subComp={subComp}>
+							{titleProp}
+						</_Heading.default>
+					</>
+				);
+		}
+	};
+
+	//RENDER
+	return <>{createheading(variant, subComp)}</>;
 };
 
-const _HeadingMixins = {};
+const _HeadingMixins = {
+	AboutPage: {
+		Main: {
+			default: `
+				position: absolute;
+				height: 100%;
+				align-self: center;
+				margin-left: 6rem;
+				border-radius: 1rem;
+				
+				box-shadow: 0 0.3rem 0.5rem 0.5rem rgba(0,0,0,0.5), 0 0.1rem 1rem 0.2rem rgba(0,0,0,0.5) inset;
+		`,
+		},
+		Content: {
+			default: `
+			padding: 1rem;
+				height: 100%;
+				width: 100%;
+				font-size: 2rem;
+				text-align: center;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				-webkit-background-clip: text;
+  			-webkit-text-fill-color: transparent;
+		`,
+		},
+	},
+};
 
 const _HeadingVariants: _HeadingVariants = {
 	default: {
@@ -41,19 +91,31 @@ const _HeadingVariants: _HeadingVariants = {
 				color: #ffffff;
     	`,
 		},
-		AboutPage: {
-			default: `
+	},
+	AboutPage: {
+		Main: {
+			default: ``,
+			defaultMaroon: `
+				background: linear-gradient(var(--About-Maroon-4), var(--About-Maroon-5));
+				${_HeadingMixins.AboutPage.Main.default}	
+			`,
+			defaultPurple: `
+			`,
+			defaultGreen: `
+			`,
+		},
+		Content: {
+			default: ``,
+			defaultMaroon: `
 				background: linear-gradient(var(--About-Maroon-1), var(--About-Maroon-2));
-				position: absolute;
-				height: fit-content;
-				width: 22.5rem;
-				align-self: center;
-				font-size: 2.85rem;
-				text-align: center;
-				-webkit-background-clip: text;
-  			-webkit-text-fill-color: transparent;
 				text-shadow: 0 0.3rem 0.5rem var(--About-Maroon-3);
-				margin-left: 5.5rem;
+				${_HeadingMixins.AboutPage.Content.default}			
+			`,
+			defaultPurple: `
+				${_HeadingMixins.AboutPage.Content.default}		
+			`,
+			defaultGreen: `
+				${_HeadingMixins.AboutPage.Content.default}		
 			`,
 		},
 	},
@@ -66,6 +128,14 @@ const _Heading = {
 				p.subComp as keyof _HeadingVariants
 			]}
 	`,
+	aboutPage: {
+		Main: styled(motion.div)<_HeadingProps>`
+			${(p) => _HeadingVariants.AboutPage.Main[p.subComp as keyof _HeadingVariants]}
+		`,
+		Content: styled(motion.h1)<_HeadingProps>`
+			${(p) => _HeadingVariants.AboutPage.Content[p.subComp as keyof _HeadingVariants]}
+		`,
+	},
 };
 
 export default Heading;
