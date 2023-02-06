c91a442cf808a414b61adb3875e2ce2153146fc9
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-05 19:12:19 -0700

    Update AboutPageThemes.tsx

--- src/styles/themes/AboutPageThemes.tsx
+++ src/styles/themes/AboutPageThemes.tsx
@@ -7,22 +7,28 @@ export const AboutPageTheme = (variant: string = "ALittleBitAboutMePage") => {
 
 	const styleObj: styleObj = {
 		ALittleBitAboutMePage: {
-			background: "linear-gradient(var(--About-Maroon-4), var(--About-Maroon-5))",
-			color: "var(--About-Maroon-4)",
-			shadow3: "var(--About-Maroon-3)",
-			shadow2: "var(--About-Maroon-2)",
+			color1: "var(--About-Maroon-1)",
+			color2: "var(--About-Maroon-2)",
+			color3: "var(--About-Maroon-3)",
+			color4: "var(--About-Maroon-4)",
+			color5: "var(--About-Maroon-5)",
+			textColor1: "var(--About-Maroon-Text-1)",
 		},
 		ALittleBitAboutTheSite: {
-			background: "linear-gradient(var(--About-Purple-1), var(--About-Purple-2))",
-			color: "var(--About-Purple-1)",
-			shadow3: "var(--About-Purple-3)",
-			shadow2: "var(--About-Purple-2)",
+			color1: "var(--About-Purple-1)",
+			color2: "var(--About-Purple-2)",
+			color3: "var(--About-Purple-3)",
+			color4: "var(--About-Purple-4)",
+			color5: "var(--About-Purple-5)",
+			textColor1: "var(--About-Purple-Text-1)",
 		},
 		ALittleBitAboutTheSourcesAndInspirations: {
-			background: "linear-gradient(var(--About-SwampGreen-1), var(--About-SwampGreen-2))",
-			color: "var(--About-SwampGreen-1)",
-			shadow3: "var(--About-SwampGreen-3)",
-			shadow2: "var(--About-SwampGreen-2)",
+			color1: "var(--About-SwampGreen-1)",
+			color2: "var(--About-SwampGreen-2)",
+			color3: "var(--About-SwampGreen-3)",
+			color4: "var(--About-SwampGreen-4)",
+			color5: "var(--About-SwampGreen-5)",
+			textColor1: "var(--About-SwampGreen-Text-1)",
 		},
 	};
 
