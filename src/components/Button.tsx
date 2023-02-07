a01decdce2e93099507849f1fe17133cb0833506
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-06 22:03:30 -0700

    Button: refactored

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,253 +1,244 @@
-import React, { useMemo } from "react";
+import React from "react";
 import styled, { useTheme } from "styled-components";
 import { motion } from "framer-motion";
 import { nanoid } from "nanoid";
 import { RxDropdownMenu } from "react-icons/Rx";
 
 import { device } from "../styles/breakpoints";
+import { selectToggle, toggle } from "../store/slices/appToggleSlice";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { selectPagesInfo, aboutTopicNavToggle } from "../store/slices/pagesInfoSlice";
 
 interface ButtonProps {
-	nameProp: string;
-	variant?: string;
-	disabled?: boolean;
-	toggleState?: boolean;
+	variant: string;
 	clickHandle?: () => void;
-	mainTheme?: {};
+	children?: React.ReactNode | React.ReactNode[] | string;
 }
 
 interface _ButtonProps {
-	variant: string;
-	mainTheme: {};
 	toggleState?: boolean;
 }
 
-interface _ButtonVariants {
-	default: any;
-	AppToggle: any;
-}
-
 interface _MotionVariants {
-	default: any;
-	AboutPage: any;
-	AboutPageNavButton: any;
-	AppToggle: any;
+	[key: string]: any;
 }
 
 // COMPONENT
-const motionDefaultTheme = {
-	background: "",
-	color: "",
-	shadow: "",
-	shadow2: "",
-};
-
 const Button = (props: ButtonProps) => {
+	// PROPS
 	const {
-		nameProp,
+		children = <></>,
 		clickHandle = () => {
 			console.log("No function");
 		},
-		toggleState = false,
-		disabled = false,
-		variant = "default",
-		mainTheme = {},
+		variant,
 	} = props;
 
-	const motionTheme = useTheme() === undefined ? motionDefaultTheme : useTheme();
+	//REDUX
+	const toggleStateApp = useAppSelector(selectToggle);
+	const toggleStateAbout = useAppSelector(selectPagesInfo).About.NavmenuToggleState;
+	const dispatch = useAppDispatch();
+	const theme = useTheme();
 
-	const createVariant = (nameProp: string, variant: string) => {
+	// STYLING LOGIC
+	const createVariant = (variant: string) => {
+		let motionProps: any = {
+			animate: "animate",
+			initial: "initial",
+			exit: "exit",
+		};
+
 		switch (variant) {
 			case "AppToggle":
-				return useMemo(
-					() => (
-						<_Button.AppToggle.slider
-							{..._MotionProps(variant, toggleState, "slider")}
+				motionProps = { ...motionProps, animate: toggleStateApp ? "on" : "off" };
+
+				return (
+					<>
+						<AppToggleSlider
+							{...motionProps}
+							variants={_MotionVariants(theme).AppToggle.slider}
 							key={nanoid()}
-							onClick={clickHandle}
-							toggleState={toggleState}
-							mainTheme={mainTheme}
+							onClick={() => {
+								dispatch(toggle());
+							}}
+							toggleState={toggleStateApp}
 						>
-							<_Button.AppToggle.button
-								{..._MotionProps(variant, toggleState, "button")}
+							<AppToggleButton
+								{...motionProps}
+								variants={_MotionVariants(theme).AppToggle.button}
 								key={nanoid()}
-								toggleState={toggleState}
-								mainTheme={mainTheme}
-							></_Button.AppToggle.button>
-						</_Button.AppToggle.slider>
-					),
-					[toggleState]
+								toggleState={toggleStateApp}
+							></AppToggleButton>
+						</AppToggleSlider>
+					</>
 				);
 			case "AboutPageNavButton":
+				motionProps = {
+					...motionProps,
+					initial: "initial",
+					animate: toggleStateAbout ? "toggleOn" : "toggleOff",
+					whileHover: "whileHover",
+					whileTap: "whileTap",
+				};
+
 				return (
-					<_Button.default
+					<AboutPageNavButton
+						{...motionProps}
+						variants={_MotionVariants(theme).AboutPageNavButton}
 						key={nanoid()}
-						{..._MotionProps(variant, toggleState, "", motionTheme)}
-						onClick={clickHandle}
-						toggleState={toggleState}
-						disabled={disabled}
-						variant={variant}
+						onClick={() => {
+							dispatch(aboutTopicNavToggle());
+						}}
+						toggleState={toggleStateAbout}
 					>
 						<RxDropdownMenu style={{ height: "4rem", width: "4rem" }} />
-					</_Button.default>
+					</AboutPageNavButton>
+				);
+			case "NavButton":
+				return (
+					<GlassButton
+						key={nanoid()}
+						{...motionProps}
+						onClick={clickHandle}
+						toggleState={toggleStateAbout}
+						disabled={true} //replace using context value
+						variant={variant}
+					>
+						{children}
+					</GlassButton>
+				);
+			case "GlassButton":
+				return (
+					<GlassButton key={nanoid()} {...motionProps} onClick={clickHandle}>
+						{children}
+					</GlassButton>
+				);
+			case "AboutPage":
+				return (
+					<AboutPage
+						key={nanoid()}
+						variants={_MotionVariants(theme).AboutPage}
+						onClick={clickHandle}
+					>
+						{children}
+					</AboutPage>
 				);
 			default:
-				return (
-					<_Button.default
-						key={nanoid()}
-						{..._MotionProps(variant, toggleState, "")}
-						onClick={clickHandle}
-						toggleState={toggleState}
-						disabled={disabled}
-						variant={variant}
-						mainTheme={mainTheme}
-					>
-						{nameProp}
-					</_Button.default>
-				);
+				return <></>;
 		}
 	};
 
 	//RENDER
-	return <>{createVariant(nameProp, variant)}</>;
+	return <>{createVariant(variant)}</>;
 };
 
 // STYLES
+const GlassButton = styled(motion.button)<_ButtonProps>`
+	background: transparent;
+	height: 6.5rem;
+	width: 20rem;
+	border: 0.5rem solid rgba(255, 255, 255, 0.7);
+	border-bottom: transparent;
+	border-radius: 1.5rem;
 
-const _ButtonVariants = (
-	toggleState?: boolean,
-	theme: any = motionDefaultTheme
-): _ButtonVariants => {
+	backdrop-filter: blur(1rem);
+	box-shadow: 0rem 0rem 0.5rem 0.1rem rgba(255, 255, 255, 0.5);
+
+	color: rgba(255, 255, 255, 0.8);
+	font-size: 3rem;
+	text-shadow: 0rem 0.1rem 0.5rem rgba(255, 255, 255, 0.5);
+	flex-shrink: 0;
+`;
+
+const AboutPage = styled(motion.button)<_ButtonProps>`
+	${({ theme }) => `
+		background: linear-gradient(${theme.color3}, ${theme.color2});
+		height: fit-content;
+		border: 0.1rem solid ${theme.color2};
+		backdrop-filter: blur(1rem);
+
+		color: ${theme.textColor1};
+		text-shadow: 0 0.2rem 0.8rem ${theme.color2};
+		font-size: 2.5rem;
+
+		// separate animation since adding motion props will decouple orchestration for this child component
+		&:hover {
+			background: ${theme.color1};
+			animation: hoverButton 0.3s forwards;
+
+			@keyframes hoverButton {
+				from {
+					transform: translateY(0rem);
+				}
+				to {
+					transform: translateY(-0.5rem);
+					box-shadow: 0 1rem 0.5rem 0.1rem rgba(10, 10, 10, 1);
+					text-shadow: 0 0.2rem 0.8rem ${theme.color4};
+				}
+			}
+		}
+
+		// separate animation since adding motion props will decouple orchestration for this child component
+		&: active {
+			animation: clickButton 0.3s forwards;
+
+			@keyframes clickButton {
+				from {
+					transform: translateY(0rem);
+					box-shadow: 0 1rem 0.5rem 0.1rem rgba(10, 10, 10, 1);
+				}
+				to {
+					transform: translateY(0rem);
+					box-shadow: none;
+				}
+			}
+		}
+	`}
+`;
+
+const AboutPageNavButton = styled(motion.button)<_ButtonProps>`
+	${({ theme }) => `
+		background: linear-gradient(${theme.color4}, ${theme.color5});
+		height: 6.5rem;
+		width: 5rem;
+		border: none;
+		svg {
+			color: ${theme.color4};
+		}
+	`}
+`;
+
+const AppToggleSlider = styled(motion.button)<_ButtonProps>`
+	${({ toggleState }) => `
+		min-height: 7rem;
+		min-width: 13rem;
+		display: flex;
+		flex-direction: column;
+		box-shadow: ${
+			toggleState
+				? "0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.23)"
+				: "inset -0.1rem -0.1rem 0.1rem rgba(0, 0, 0, 0.23), inset 0.1rem 0.1rem 0.1rem rgba(255, 255, 255, 0.21)"
+		} ;
+		border-radius: 14rem;
+		justify-content: center;
+		padding: 0 0.8rem;
+	`}
+`;
+
+const AppToggleButton = styled(motion.span)<_ButtonProps>`
+	${({ toggleState }) => `
+		height: 3rem;
+		width: 3rem;
+		border-radius: 50%;
+		border: none;
+		box-shadow: ${toggleState ? "0.2rem 0.2rem 0.2rem rgba(0,0,0,0.5)" : ""};
+		filter: blur(0.1rem);
+		transform: translateX(0rem);
+	`}
+`;
+
+const _MotionVariants = (theme?: any): _MotionVariants => {
 	return {
-		default: {
-			default: ``,
-			glassButton: `
-				background: transparent;
-				height: 6.5rem;
-				width: 20rem;
-				border: 0.5rem solid rgba(255,255,255,0.7);
-				border-bottom: transparent;
-				border-radius: 1.5rem;
-				
-				backdrop-filter: blur(1rem);
-				box-shadow: 0rem 0rem 0.5rem 0.1rem rgba(255,255,255,0.5);
-				
-				color: rgba(255,255,255,0.8);
-				font-size: 3rem;
-				text-shadow: 0rem 0.1rem 0.5rem rgba(255,255,255,0.5);
-				flex-shrink: 0;
-			`,
-			AboutPage: `
-				background: linear-gradient(${theme.color3}, ${theme.color2});
-				height: fit-content;
-				border: 0.1rem solid ${theme.color2};
-				backdrop-filter: blur(1rem);
-				
-				color: ${theme.textColor1};
-				text-shadow: 0 0.2rem 0.8rem ${theme.color2};
-				font-size: 2.5rem;
-				
-				// separate animation since adding motion props will decouple orchestration for this child component
-				&:hover { 
-					background: ${theme.color1};
-					animation: hoverButton 0.3s forwards;
-
-					@keyframes hoverButton {
-						from {
-							transform: translateY(0rem);
-						}
-						to {
-							transform: translateY(-0.5rem);
-							box-shadow: 0 1rem 0.5rem 0.1rem rgba(10, 10, 10, 1);
-							text-shadow: 0 0.2rem 0.8rem ${theme.color4};
-						}
-					}
-				}
-
-				// separate animation since adding motion props will decouple orchestration for this child component
-				&: active {
-					animation: clickButton 0.3s forwards;
-
-					@keyframes clickButton {
-						from {
-							transform: translateY(0rem);
-							box-shadow: 0 1rem 0.5rem 0.1rem rgba(10, 10, 10, 1);
-						}
-						to {
-							transform: translateY(0rem);
-							box-shadow: none;
-						}
-					}
-				}
-			`,
-			AboutPageNavButton: `
-				background: linear-gradient(${theme.color4}, ${theme.color5});
-				height: 6.5rem;
-				width: 5rem;
-				border: none;
-				svg {
-					color: ${theme.color4};
-				}
-			`,
-		},
-		AppToggle: {
-			slider: `
-      min-height: 7rem;
-      min-width: 13rem;
-      display: flex;
-      flex-direction: column;
-      box-shadow: ${
-				toggleState
-					? "0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.23)"
-					: "inset -0.1rem -0.1rem 0.1rem rgba(0, 0, 0, 0.23), inset 0.1rem 0.1rem 0.1rem rgba(255, 255, 255, 0.21)"
-			} ;
-      border-radius: 14rem;
-      justify-content: center;
-      padding: 0 0.8rem;
-
-      @media ${device.tablet}{
-				
-      }
-
-      @media ${device.laptop}{
-				
-      }
-    `,
-			button: `
-      height: 3rem;
-      width: 3rem;
-      border-radius: 50%;
-      border: none;
-			box-shadow: ${toggleState ? "0.2rem 0.2rem 0.2rem rgba(0,0,0,0.5)" : ""};
-      filter: blur(0.1rem);
-			transform: translateX(0rem);
-    `,
-		},
-	};
-};
-
-const _Button: _ButtonVariants = {
-	default: styled(motion.button)<_ButtonProps>`
-		${(p) => _ButtonVariants(p.toggleState, p.theme).default[p.variant as keyof _ButtonVariants]}
-	`,
-	AppToggle: {
-		slider: styled(motion.button)<_ButtonProps>`
-			${(p) => _ButtonVariants(p.toggleState).AppToggle.slider}
-		`,
-		button: styled(motion.span)<_ButtonProps>`
-			${(p) => _ButtonVariants(p.toggleState).AppToggle.button}
-		`,
-	},
-};
-
-// MOTION
-
-const _MotionVariants = (
-	variant: string,
-	subComp: string = "",
-	theme: any = motionDefaultTheme
-): _MotionVariants => {
-	const motionObj: _MotionVariants = {
-		default: {},
 		AboutPage: {
 			initial: {
 				opacity: 0,
@@ -308,18 +299,17 @@ const _MotionVariants = (
 		AppToggle: {
 			slider: {
 				on: {
-					background: "#f1f1f1",
-					border: "1.5rem solid #383838",
+					background: `${theme.color1}`,
+					border: `1.5rem solid ${theme.color3}`,
 				},
 				off: {
-					background: "#383838",
-					border: "1.5rem solid #FFFFFF",
+					background: "rgb(56, 56, 56)",
+					border: "1.5rem solid rgb(255, 255, 255)",
 				},
 			},
 			button: {
 				on: {
-					background:
-						"radial-gradient(circle at 30% 40%, rgba(0, 0, 0, 0.5) 0%, rgba(255,255,255,0.8) 50%)",
+					background: `radial-gradient(circle at 30% 40%, ${theme.color3} 0%, ${theme.color1} 50%)`,
 					transform: "translateX(5.5rem)",
 					transition: {
 						duration: 0.5,
@@ -338,41 +328,6 @@ const _MotionVariants = (
 			},
 		},
 	};
-
-	return subComp === ""
-		? motionObj[variant as keyof _MotionVariants]
-		: motionObj[variant as keyof _MotionVariants][subComp as keyof _MotionVariants];
-};
-
-const _MotionProps = (variant: string, toggleState?: boolean, subComp?: string, theme?: any) => {
-	let returnProps: any = {
-		variants: _MotionVariants,
-	};
-
-	switch (variant) {
-		case "AppToggle":
-			returnProps = { ...returnProps, animate: toggleState ? "on" : "off" };
-			returnProps.variants = _MotionVariants(variant, subComp);
-			break;
-		case "AboutPage": //orchestrated by Navmenu _MotionProps 'AboutPage' variant
-			returnProps.variants = _MotionVariants("AboutPage");
-			break;
-		case "AboutPageNavButton":
-			returnProps = {
-				...returnProps,
-				initial: "initial",
-				animate: toggleState ? "toggleOn" : "toggleOff",
-				whileHover: "whileHover",
-				whileTap: "whileTap",
-			};
-			returnProps.variants = _MotionVariants("AboutPageNavButton", "", theme);
-			break;
-		default:
-			returnProps = { ...returnProps, animate: "animate", initial: "initial", exit: "exit" };
-			break;
-	}
-
-	return returnProps;
 };
 
 export default Button;
