7da67a48fd55e214e2ce520cbfc38d7e23b497e2
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-06 22:04:41 -0700

    navmenu: refactored

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,164 +1,114 @@
 import React from "react";
-import styled, { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider, useTheme } from "styled-components";
 import { AnimatePresence, motion } from "framer-motion";
 import { nanoid } from "nanoid";
 import Button from "./Button";
-import { AboutPageTheme } from "../styles/themes/AboutPageThemes";
+import { useAppSelector } from "../store/hooks";
+import { selectPagesInfo } from "../store/slices/pagesInfoSlice";
 
 interface NavmenuProps {
-	children?: React.ReactNode[] | React.ReactNode;
-	toggleState?: boolean;
+	children: React.ReactNode[] | React.ReactNode;
 	variant?: string;
-	subComp?: string;
-	clickHandle?: Function;
-}
-
-interface _NavmenuProps {
-	variant: string;
-	subComp: string;
-}
-
-interface _NavVariants {
-	default: any;
-	AboutPage: any;
 }
 
 interface _MotionVariants {
-	default: any;
-	AboutPage: any;
+	[key: string]: any;
 }
 
 const Navmenu = (props: NavmenuProps) => {
-	const {
-		children = undefined,
-		variant = "default",
-		subComp = "Main",
-		toggleState = false,
-		clickHandle = () => {},
-	} = props;
+	const { children, variant = "default" } = props;
 
-	const createVariant = () => {
+	const toggleStateAbout = useAppSelector(selectPagesInfo).About.NavmenuToggleState;
+	const theme = useTheme();
+
+	const createVariant = (variant: string) => {
+		let motionProps: any = {
+			animate: "animate",
+			initial: "initial",
+			exit: "exit",
+		};
+
 		switch (variant) {
 			case "AboutPage":
+				motionProps = { ...motionProps };
+				motionProps = {
+					...motionProps,
+					initial: "initial",
+					animate: toggleStateAbout ? "toggleOn" : "",
+					exit: "toggleOff",
+				};
+
 				return (
 					<AnimatePresence mode="wait">
-						<_Nav.AboutPage key={nanoid()} variant={variant} subComp={"Main"}>
-							<_Nav.AboutPage
+						<AboutPageMain key={nanoid()}>
+							<AboutPageDropdownList
+								{...motionProps}
 								key={nanoid()}
-								{..._MotionProps(variant, toggleState)}
-								subComp={"DropdownList"}
-								variant={variant}
+								variants={_MotionVariants(theme).AboutPage}
 							>
 								{children}
-							</_Nav.AboutPage>
-							<Button
-								nameProp={""}
-								variant={"AboutPageNavButton"}
-								toggleState={toggleState}
-								clickHandle={() => {
-									clickHandle();
-								}}
-							/>
-						</_Nav.AboutPage>
+							</AboutPageDropdownList>
+							<Button variant={"AboutPageNavButton"} />
+						</AboutPageMain>
 					</AnimatePresence>
 				);
 			default:
-				return (
-					<_Nav.default variant={variant} subComp={subComp}>
-						{children}
-					</_Nav.default>
-				);
+				return <Main key={nanoid()}>{children}</Main>;
 		}
 	};
 
-	return <>{createVariant()}</>;
+	return <>{createVariant(variant)}</>;
 };
 
 //STYLE
 
-const _NavVariants: _NavVariants = {
-	default: {
-		Main: `
-      height: fit-content;
-      width: 100%;
-      display: flex; 
-      justify-content: space-between;
-      padding-right: 10%;
-      column-gap: 2%;
-			margin-bottom: 10rem;
-    `,
-	},
-	AboutPage: {
-		Main: `
-			height: fit-content;
-      width: 100%;
-			display: flex;
-			position: relative;
-			padding: 0.3rem; // to prevent heading from pushing down
-			margin: auto 0;
-			z-index: 4;
-		`,
-		DropdownList: `
-			height: fit-content;
-      width: fit-content;
-			display: flex;
-			flex-direction: column;
-		`,
-	},
-};
+const Main = styled(motion.div)`
+	height: fit-content;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	padding-right: 10%;
+	column-gap: 2%;
+	margin-bottom: 10rem;
+`;
 
-const _Nav = {
-	default: styled(motion.div)<_NavmenuProps>`
-		${(p) => _NavVariants.default[p.subComp as keyof _NavVariants]}
-	`,
-	AboutPage: styled(motion.div)<_NavmenuProps>`
-		${(p) => _NavVariants.AboutPage[p.subComp as keyof _NavVariants]}
-	`,
-};
+const AboutPageMain = styled(motion.div)`
+	height: fit-content;
+	width: 100%;
+	display: flex;
+	position: relative;
+	padding: 0.3rem; // to prevent heading from pushing down
+	margin: auto 0;
+	z-index: 4;
+`;
 
-//Motion
+const AboutPageDropdownList = styled(motion.div)`
+	height: fit-content;
+	width: fit-content;
+	display: flex;
+	flex-direction: column;
+`;
 
-const _MotionVariants = {
-	default: {},
-	AboutPage: {
-		initial: {},
-		toggleOff: {
-			transition: {
-				duration: 0.5,
-				staggerChildren: 0.3,
-				staggerDirection: -1,
+//MOTION
+const _MotionVariants = (theme?: any): _MotionVariants => {
+	return {
+		AboutPage: {
+			initial: {},
+			toggleOff: {
+				transition: {
+					duration: 0.5,
+					staggerChildren: 0.3,
+					staggerDirection: -1,
+				},
+			},
+			toggleOn: {
+				transition: {
+					duration: 0.5,
+					staggerChildren: 0.3,
+				},
 			},
 		},
-		toggleOn: {
-			transition: {
-				duration: 0.5,
-				staggerChildren: 0.3,
-			},
-		},
-	},
-};
-
-const _MotionProps = (variant: string, toggleState?: boolean) => {
-	let returnProps: any = {
-		variants: _MotionVariants,
 	};
-
-	switch (variant) {
-		case "AboutPage":
-			returnProps = { ...returnProps };
-			returnProps = {
-				...returnProps,
-				initial: "initial",
-				animate: toggleState ? "toggleOn" : "",
-				exit: "toggleOff",
-			};
-			returnProps.variants = _MotionVariants.AboutPage;
-			break;
-		default:
-			break;
-	}
-
-	return returnProps;
 };
 
 export default Navmenu;
