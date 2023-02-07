186c42c3cd82fe98f1255bc44b4efc2dd08399fc
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-06 22:04:31 -0700

    Heading: refactored

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,127 +5,71 @@ import { device } from "../styles/breakpoints";
 
 interface HeadingProps {
 	titleProp: string;
-	subComp?: string;
-	variant?: string;
-}
-
-interface _HeadingProps {
 	variant: string;
-	subComp: string;
-}
-
-interface _HeadingVariants {
-	default: any;
-	AboutPage: any;
 }
 
 const Heading = (props: HeadingProps) => {
-	const { titleProp, variant = "default", subComp = "default" } = props;
+	const { titleProp, variant = "default" } = props;
 
-	const createheading = (variant: string, subComp: string) => {
+	const createVariant = (variant: string) => {
 		switch (variant) {
 			case "AboutPage":
 				return (
 					<>
-						<_Heading.aboutPage.Main variant={variant} subComp={subComp}>
-							<_Heading.aboutPage.Content variant={variant} subComp={subComp}>
-								{titleProp}
-							</_Heading.aboutPage.Content>
-						</_Heading.aboutPage.Main>
+						<AboutPageMain>
+							<AboutPageContent>{titleProp}</AboutPageContent>
+						</AboutPageMain>
 					</>
 				);
+			case "Landing":
+				return <Landing>{titleProp}</Landing>;
 			default:
-				return (
-					<>
-						<_Heading.default variant={variant} subComp={subComp}>
-							{titleProp}
-						</_Heading.default>
-					</>
-				);
+				return;
+				<></>;
 		}
 	};
 
 	//RENDER
-	return <>{createheading(variant, subComp)}</>;
+	return <>{createVariant(variant)}</>;
 };
 
-const _HeadingMixins = {
-	AboutPage: {
-		Main: {
-			default: `
-				position: absolute;
-				height: 100%;
-				align-self: center;
-				margin-left: 6rem;
-				border-radius: 1rem;
-				
-				box-shadow: 0 0.3rem 0.5rem 0.5rem rgba(0,0,0,0.5), 0 0.1rem 1rem 0.2rem rgba(0,0,0,0.5) inset;
-		`,
-		},
-		Content: {
-			default: `
-			padding: 1rem;
-				height: 100%;
-				width: 100%;
-				font-size: 2rem;
-				text-align: center;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				-webkit-background-clip: text;
-  			-webkit-text-fill-color: transparent;
-		`,
-		},
-	},
-};
+const Landing = styled(motion.h1)`
+	margin-top: 5rem;
+	font-weight: 200;
+	font-size: 4.7rem;
+	text-align: center;
+	color: #ffffff;
+`;
 
-const _HeadingVariants = (theme: any): _HeadingVariants => {
-	return {
-		default: {
-			default: ``,
-			Landing: {
-				default: `
-				margin-top: 5rem;
-				font-weight: 200;
-				font-size: 4.7rem;
-				text-align: center;
-				color: #ffffff;
-    	`,
-			},
-		},
-		AboutPage: {
-			Main: {
-				default: `
-					background: linear-gradient(${theme.color4}, ${theme.color5});
-					${_HeadingMixins.AboutPage.Main.default}
-				`,
-			},
-			Content: {
-				default: `
-					background: linear-gradient(${theme.color1}, ${theme.color2});
-					text-shadow: 0 0.3rem 0.4rem rgba(0,0,0,0.4);
-					${_HeadingMixins.AboutPage.Content.default}		
-				`,
-			},
-		},
-	};
-};
+const AboutPageMain = styled(motion.div)`
+	${({ theme }) => `
+		background: linear-gradient(${theme.color4}, ${theme.color5});
+		position: absolute;
+		height: 100%;
+		align-self: center;
+		margin-left: 6rem;
+		border-radius: 1rem;
 
-const _Heading = {
-	default: styled(motion.h1)<_HeadingProps>`
-		${(p) =>
-			_HeadingVariants(p.theme).default[p.variant as keyof _HeadingVariants][
-				p.subComp as keyof _HeadingVariants
-			]}
-	`,
-	aboutPage: {
-		Main: styled(motion.div)<_HeadingProps>`
-			${(p) => _HeadingVariants(p.theme).AboutPage.Main[p.subComp as keyof _HeadingVariants]}
-		`,
-		Content: styled(motion.h1)<_HeadingProps>`
-			${(p) => _HeadingVariants(p.theme).AboutPage.Content[p.subComp as keyof _HeadingVariants]}
-		`,
-	},
-};
+		box-shadow: 0 0.3rem 0.5rem 0.5rem rgba(0, 0, 0, 0.5),
+			0 0.1rem 1rem 0.2rem rgba(0, 0, 0, 0.5) inset;
+	`}
+`;
+
+const AboutPageContent = styled(motion.div)`
+	${({ theme }) => `
+		background: linear-gradient(${theme.color1}, ${theme.color2});
+		text-shadow: 0 0.3rem 0.4rem rgba(0, 0, 0, 0.4);
+		padding: 1rem;
+		height: 100%;
+		width: 100%;
+		font-size: 2rem;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	`}
+`;
 
 export default Heading;
