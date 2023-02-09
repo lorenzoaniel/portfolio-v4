8702d453a5909ba19d0249da7f669a9b53b4f800
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-08 22:00:20 -0700

    Contact: added majority of components
    just needs fine tunning on styling and then adding click functionality for the actul links themselves

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,23 +1,113 @@
+import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
+import { SiGithub, SiLinkedin } from "react-icons/Si";
+import { HiBadgeCheck } from "react-icons/Hi";
+import Paragraph from "../components/Paragraph";
+
+//LinkedIn, creditly badge, Github,
 
 const Contact = () => {
-	return <_Contact.Main>Contact</_Contact.Main>;
+	const tempIconStyle = {
+		height: "100%",
+		width: "100%",
+	};
+
+	return (
+		<Main>
+			<LinkContainer>
+				<IconContainer>
+					<SiGithub style={tempIconStyle} />
+				</IconContainer>
+				<Paragraph
+					variant={"ContactPageLink"}
+					data={"https://www.linkedin.com/in/mikhail-lorenzo-aniel-283022127/"}
+				/>
+			</LinkContainer>
+			<LinkContainer>
+				<IconContainer>
+					<SiLinkedin style={tempIconStyle} />
+				</IconContainer>
+				<Paragraph
+					variant={"ContactPageLink"}
+					data={"https://www.linkedin.com/in/mikhail-lorenzo-aniel-283022127/"}
+				/>
+			</LinkContainer>
+			<LinkContainer>
+				<IconContainer>
+					<HiBadgeCheck style={tempIconStyle} />
+				</IconContainer>
+				<Paragraph
+					variant={"ContactPageLink"}
+					data={"https://www.linkedin.com/in/mikhail-lorenzo-aniel-283022127/"}
+				/>
+			</LinkContainer>
+		</Main>
+	);
 };
 
-const _Contact = {
-	Main: styled.section`
-		/* background-image: linear-gradient(to left, rgba(255, 255, 255, 0.2), rgba(230, 230, 230, 0.5)); */
-		width: 100%;
-		height: 15rem;
-		color: white;
-		font-size: 3rem;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		align-self: flex-end;
-	`,
-};
+const Main = styled(motion.section)`
+	background: linear-gradient(
+		to left,
+		var(--Contact-Yellow-1),
+		var(--Contact-Yellow-2),
+		var(--Contact-Yellow-3)
+	);
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	align-self: flex-end;
+	border-radius: 2rem;
+	padding: 1.5rem;
+	box-shadow: 0 0 1rem 0.5rem var(--Contact-Yellow-5), 0 0 1rem 0.5rem var(--Contact-Yellow-4) inset;
+	backdrop-filter: blur(1rem);
+	overflow-y: scroll; //since it is flex row
+	scrollbar-width: thin;
+	row-gap: 1rem;
+
+	color: black;
+	font-size: 3rem;
+`;
+
+const LinkContainer = styled(motion.div)`
+	/* background: yellow; */
+	width: 100%;
+	height: fit-content;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	row-gap: 1rem;
+
+	background: linear-gradient(
+		to left,
+		var(--Contact-DarkBlue-5),
+		var(--Contact-DarkBlue-3),
+		var(--Contact-DarkBlue-1)
+	);
+	box-shadow: 0 0.3rem 0.5rem 0.5rem rgba(0, 0, 0, 0.5),
+		0 0.1rem 1rem 0.2rem rgba(0, 0, 0, 0.5) inset;
+
+	border-radius: 1rem;
+	padding: 2rem;
+`;
+
+const IconContainer = styled(motion.div)`
+	background: linear-gradient(var(--Contact-Yellow-3), var(--Contact-Yellow-5));
+	height: clamp(10rem, 50%, 30rem);
+	width: clamp(10rem, 50%, 30rem);
+	box-shadow: 0 0 1rem 0.3rem var(--Contact-Yellow-5), 0 0.1rem 1rem 0.2rem rgba(0, 0, 0, 0.5) inset;
+	border-radius: 1rem;
+	border-width: 1rem;
+	border-style: solid;
+	border-image: linear-gradient(var(--Contact-Yellow-1), var(--Contact-Yellow-2));
+	padding: 1rem;
+`;
+
+const CenterPiece = styled(motion.div)``;
 
 export default Contact;
