06519a49d7a01bf9d9b05091028d59fedd50f957
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-09 20:39:00 -0700

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,23 +1,49 @@
+import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
 
 const Projects = () => {
-	return <_Projects.Main>Project</_Projects.Main>;
+	return (
+		<Main>
+			<ProjectMain />
+			<ProjectMain />
+			<ProjectMain />
+			<ProjectMain />
+		</Main>
+	);
 };
 
-const _Projects = {
-	Main: styled.section`
-		/* background-image: linear-gradient(to left, rgba(255, 255, 255, 0.2), rgba(230, 230, 230, 0.5)); */
-		width: 100%;
-		height: 15rem;
-		color: white;
-		font-size: 3rem;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		align-self: flex-end;
-	`,
-};
+const Main = styled(motion.section)`
+	background: linear-gradient(
+		to left,
+		var(--Projects-Orange-1),
+		var(--Projects-Orange-2),
+		var(--Projects-Orange-3)
+	);
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	align-self: flex-end;
+	border-radius: 2rem;
+	padding: 1.5rem;
+	row-gap: 5rem;
+	box-shadow: 0 0 1rem 0.5rem var(--Projects-Orange-5),
+		0 0 1rem 0.5rem var(--Projects-Orange-4) inset;
+	backdrop-filter: blur(1rem);
+	overflow-y: scroll; //since it is flex row
+	scrollbar-width: thin;
+
+	color: black;
+	font-size: 3rem;
+`;
+
+const ProjectMain = styled(motion.div)`
+	background: green;
+	min-height: 25rem;
+	width: 25rem;
+`;
 
 export default Projects;
