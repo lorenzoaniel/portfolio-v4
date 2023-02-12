2c24020c41a5a991051afb06dda74ac5c9b63fc2
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-12 00:10:51 -0700

    Heading: added ProjectMainHeading variant
    used to show user that they are in on the projects page

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -23,12 +23,19 @@ const Heading = (props: HeadingProps) => {
 				);
 			case "Landing":
 				return <Landing>{titleProp}</Landing>;
+			case "ProjectMainHeading":
+				return (
+					<ProjectsMain>
+						<ProjectsContent>{titleProp}</ProjectsContent>
+					</ProjectsMain>
+				);
 			default:
 				return;
 				<></>;
 		}
 	};
 
+	console.log("Heading rerendered!");
 	//RENDER
 	return <>{createVariant(variant)}</>;
 };
@@ -70,6 +77,33 @@ const AboutPageContent = styled(motion.div)`
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
 	`}
+`;
+
+const ProjectsMain = styled(motion.div)`
+	background: linear-gradient(var(--Projects-Indigo-1), var(--Projects-Indigo-3));
+	height: fit-content;
+	width: fit-content;
+	border-radius: 1rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 1rem;
+	border: 1rem groove var(--Projects-Indigo-5);
+
+	box-shadow: 0 0.3rem 0.5rem 0.5rem var(--Projects-Indigo-5),
+		0 0.1rem 1rem 0.5rem var(--Projects-Indigo-5) inset;
+`;
+
+const ProjectsContent = styled(motion.div)`
+	background: var(--Projects-Indigo-5);
+	height: 100%;
+	width: 100%;
+	text-shadow: 0 0.2rem 0.3rem var(--Projects-Indigo-1);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	filter: drop-shadow(0 0.2rem 0.2rem rgba(0, 0, 0, 1));
+	font-size: 5rem;
+	font-weight: 900;
 `;
 
 export default Heading;
