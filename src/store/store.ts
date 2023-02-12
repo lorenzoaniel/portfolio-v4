d2dc6a27c651d60cc7ba94049770a7399a05ccff
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-12 00:14:52 -0700

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { appToggleSlice } from "./slices/appToggleSlice";
 import { carouselSlice } from "./slices/carouselSlice";
 import { pagesInfoSlice } from "./slices/pagesInfoSlice";
+import { aboutToggleSlice } from "./slices/aboutToggleSlice";
 
 export const store = configureStore({
 	reducer: {
+		aboutToggle: aboutToggleSlice.reducer,
 		toggle: appToggleSlice.reducer,
 		carouselSlice: carouselSlice.reducer,
 		pagesInfo: pagesInfoSlice.reducer,
