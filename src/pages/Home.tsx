a5cbd22309a2777a37900887dd0df463292695e2
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-12 00:12:50 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,29 +3,30 @@ import styled from "styled-components";
 import { useAppSelector } from "../store/hooks";
 import Paragraph from "../components/Paragraph";
 import { selectPagesInfo } from "../store/slices/pagesInfoSlice";
+import { motion } from "framer-motion";
 
 const Home = () => {
 	const infoContext = useAppSelector(selectPagesInfo);
 
+	console.log("Home rerendered!");
+	//RENDER
 	return (
-		<_Home.Main>
+		<Main>
 			<Paragraph data={infoContext.Home.Main} variant="HomePage" />
-		</_Home.Main>
+		</Main>
 	);
 };
 
-const _Home = {
-	Main: styled.section`
-		width: 100%;
-		height: 15rem;
-		color: black;
-		font-size: 3rem;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		align-self: flex-end;
-	`,
-};
+const Main = styled(motion.section)`
+	width: 100%;
+	height: 15rem;
+	color: black;
+	font-size: 3rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	align-self: flex-end;
+`;
 
 export default Home;
