26bce71386a26557de0f962c4612077d1e3f0627
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-12 00:13:21 -0700

    Projects: separated Main into two sub containers
    Headers and Content

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,61 +4,124 @@ import styled from "styled-components";
 import Paragraph from "../components/Paragraph";
 import Button from "../components/Button";
 import useCurrentDimension from "../helpers/useCurrentDimension";
+import { device } from "../styles/breakpoints";
+import Heading from "../components/Heading";
 
 const Projects = () => {
-	const currentWidth = useCurrentDimension().width;
+	let currentDimension = useCurrentDimension();
 
 	const motionProps = {
 		initial: "initial",
-		animate: currentWidth >= 1024 ? "animate" : "whileHover",
+		animate: currentDimension.width >= 820 ? "animate" : "whileHover",
 		whileHover: "whileHover",
 		exit: "exit",
 	};
 
+	console.log("Projects rerendered!");
+	//RENDER
 	return (
 		<Main>
-			<ProjectMain {...motionProps} variants={_MotionVariants().ProjectMain}>
-				<ProjectSlideUp variants={_MotionVariants().ProjectSlideUp}></ProjectSlideUp>
-				<ProjectCover>
-					<Paragraph data={"TENZIES"} variant={"ProjectCover"} />
-				</ProjectCover>
-				<ProjectSlideDown variants={_MotionVariants().ProjectSlideDown}>
-					<Button variant={"ProjectSlideDownButton"}>{"Github Code"}</Button>
-				</ProjectSlideDown>
-			</ProjectMain>
+			<Headers>
+				<Heading titleProp={"Projects"} variant={"ProjectMainHeading"} />
+			</Headers>
+			<Contents>
+				<ProjectMain {...motionProps} variants={_MotionVariants().ProjectMain}>
+					<ProjectSlideUp variants={_MotionVariants().ProjectSlideUp}>test</ProjectSlideUp>
+					<ProjectCover>
+						<Paragraph data={"TENZIES"} variant={"ProjectCover"} />
+					</ProjectCover>
+					<ProjectSlideDown variants={_MotionVariants().ProjectSlideDown}>
+						<Button variant={"ProjectSlideDownButton"}>{"Github Code"}</Button>
+					</ProjectSlideDown>
+				</ProjectMain>
+				<ProjectMain {...motionProps} variants={_MotionVariants().ProjectMain}>
+					<ProjectSlideUp variants={_MotionVariants().ProjectSlideUp}>test</ProjectSlideUp>
+					<ProjectCover>
+						<Paragraph data={"TENZIES"} variant={"ProjectCover"} />
+					</ProjectCover>
+					<ProjectSlideDown variants={_MotionVariants().ProjectSlideDown}>
+						<Button variant={"ProjectSlideDownButton"}>{"Github Code"}</Button>
+					</ProjectSlideDown>
+				</ProjectMain>
+				<ProjectMain {...motionProps} variants={_MotionVariants().ProjectMain}>
+					<ProjectSlideUp variants={_MotionVariants().ProjectSlideUp}>test</ProjectSlideUp>
+					<ProjectCover>
+						<Paragraph data={"TENZIES"} variant={"ProjectCover"} />
+					</ProjectCover>
+					<ProjectSlideDown variants={_MotionVariants().ProjectSlideDown}>
+						<Button variant={"ProjectSlideDownButton"}>{"Github Code"}</Button>
+					</ProjectSlideDown>
+				</ProjectMain>
+				<ProjectMain {...motionProps} variants={_MotionVariants().ProjectMain}>
+					<ProjectSlideUp variants={_MotionVariants().ProjectSlideUp}>test</ProjectSlideUp>
+					<ProjectCover>
+						<Paragraph data={"TENZIES"} variant={"ProjectCover"} />
+					</ProjectCover>
+					<ProjectSlideDown variants={_MotionVariants().ProjectSlideDown}>
+						<Button variant={"ProjectSlideDownButton"}>{"Github Code"}</Button>
+					</ProjectSlideDown>
+				</ProjectMain>
+			</Contents>
 		</Main>
 	);
 };
 
 const Main = styled(motion.section)`
-	/* background: linear-gradient(
+	background: linear-gradient(
 		to left,
 		var(--Projects-Orange-1),
 		var(--Projects-Orange-2),
 		var(--Projects-Orange-3)
-	); */
+	);
 	width: 100%;
 	height: 100%;
+	display: flex;
+	flex-direction: column;
+	row-gap: 2%;
 
+	border-radius: 2rem;
+	padding: 3rem;
+
+	box-shadow: 0 0 1rem 0.5rem var(--Projects-Orange-5),
+		0 0 1rem 0.5rem var(--Projects-Orange-5) inset;
+	backdrop-filter: blur(1rem);
+`;
+
+const Headers = styled(motion.div)`
+	/* background: green; */
+	width: 100%;
+	height: 20%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Contents = styled(motion.div)`
+	/* background: yellow; */
+	width: 100%;
+	height: 75%;
+	overflow-y: scroll; //since it is flex row
+	scrollbar-width: thin;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	align-self: flex-end;
-	border-radius: 2rem;
-	padding: 5%;
 	row-gap: 5rem;
-	box-shadow: 0 0 1rem 0.5rem var(--Projects-Orange-5),
-		0 0 1rem 0.5rem var(--Projects-Orange-5) inset;
-	backdrop-filter: blur(1rem);
-	overflow-y: scroll; //since it is flex row
-	scrollbar-width: thin;
+	/* padding: 5%; */
 
 	color: black;
 	font-size: 3rem;
+
+	@media ${device.tablet} {
+		flex-direction: row;
+		flex-wrap: wrap;
+		column-gap: 5rem;
+		justify-content: center;
+	}
 `;
 
 const ProjectMain = styled(motion.div)`
-	width: clamp(30rem, 50%, 50rem);
+	width: clamp(30rem, 50%, 40rem);
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
@@ -67,7 +130,7 @@ const ProjectMain = styled(motion.div)`
 `;
 
 const ProjectCover = styled(motion.div)`
-	/* background: linear-gradient(var(--Projects-Indigo-1), var(--Projects-Indigo-3)); */
+	background: linear-gradient(var(--Projects-Indigo-1), var(--Projects-Indigo-3));
 	box-shadow: 0 0 1rem 0.4rem var(--Projects-Indigo-1),
 		0 0 1rem 0.4rem var(--Projects-Indigo-4) inset, 0 0 5rem 0.2rem var(--Projects-Indigo-1) inset;
 	border: 1rem groove var(--Projects-Indigo-5);
@@ -126,7 +189,6 @@ const _MotionVariants = (theme?: any) => {
 			},
 			whileHover: {
 				transform: "translateY(0%) scaleY(1)",
-				background: "none",
 			},
 		},
 		ProjectSlideDown: {
@@ -135,7 +197,6 @@ const _MotionVariants = (theme?: any) => {
 			},
 			whileHover: {
 				transform: "translateY(0%)",
-				background: "none",
 			},
 		},
 	};
