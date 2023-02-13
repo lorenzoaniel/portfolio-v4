3dd00ae14bb6cf1ce378e4d6e00f601ff4415018
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-12 22:48:07 -0700

    Decorations: moved React icons here, new spaceship variant
    moved react icons from topicspage comp and added to decorations along witha a new aniamted spaceshiop decoration

--- src/components/Decorations.tsx
+++ src/components/Decorations.tsx
@@ -1,160 +1,176 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion, useAnimationControls } from "framer-motion";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { FaSpaceShuttle } from "react-icons/Fa";
+import { RiReactjsFill } from "react-icons/Ri";
+import {
+	SiStyledcomponents,
+	SiReactrouter,
+	SiRedux,
+	SiVite,
+	SiTypescript,
+	SiGithub,
+	SiCloudways,
+} from "react-icons/Si";
+import { CgFramer } from "react-icons/Cg";
 
-interface DProps {
+interface Props {
 	variant?: string;
 }
 
-interface _DProps {
-	variant: string;
-	subComp?: string;
-}
-
-interface _DVariants {
-	default: any;
-	Main: any;
-	Spaceship: any;
-	Details: any;
-}
-
-const Decorations = (props: DProps) => {
+const Decorations = (props: Props) => {
 	const { variant = "default" } = props;
-	const DecorationConfigs = {
-		Main: {
-			variant: "Main",
-			subComp: variant,
-		},
-		Spaceship: {
-			SolarContainer: {
-				variant: "Spaceship",
-				subComp: "SolarContainer",
+
+	const createReactIconStyles = (variant: string, iconGroupAmount: number = 0) => {
+		interface styleType {
+			default: any;
+			multiple: any;
+			spaceship: any;
+		}
+
+		const dynamicWidthHeight = 50 / iconGroupAmount + "vw";
+
+		const reactIconStyles: styleType = {
+			default: {
+				minHeight: "50%",
+				minWidth: "50%",
 			},
-			BodyUpper: {
-				variant: "Spaceship",
-				subComp: "BodyUpper",
+			multiple: {
+				height: dynamicWidthHeight,
+				width: dynamicWidthHeight,
 			},
-			BodyLower: {
-				variant: "Spaceship",
-				subComp: "BodyLower",
+			spaceship: {
+				height: "15vh",
+				width: "15vh",
 			},
-			Legs: {
-				variant: "Spaceship",
-				subComp: "Legs",
-			},
-			Thruster: {
-				variant: "Spaceship",
-				subComp: "Thruster",
-			},
-		},
-		Details: {
-			Spaceship: {
-				SolarPanels: {
-					variant: "Spaceship",
-					subComp: "SolarPanels",
-				},
-			},
-		},
+		};
+
+		return reactIconStyles[variant as keyof styleType];
 	};
 
-	const createDecoration = () => {
+	const createVariant = (variant: string) => {
+		let motionProps = {};
+
 		switch (variant) {
 			case "Spaceship":
 				return (
-					<>
-						<_D.Spaceship.SolarContainer {...DecorationConfigs.Spaceship.SolarContainer}>
-							<_D.Details {...DecorationConfigs.Details.Spaceship.SolarPanels} />
-						</_D.Spaceship.SolarContainer>
-						<_D.Spaceship.Body {...DecorationConfigs.Spaceship.BodyUpper} />
-						<_D.Spaceship.Body {...DecorationConfigs.Spaceship.BodyLower} />
-						<_D.Spaceship.Legs {...DecorationConfigs.Spaceship.Legs} />
-						<_D.Spaceship.Legs {...DecorationConfigs.Spaceship.Legs} />
-						<_D.Spaceship.Legs {...DecorationConfigs.Spaceship.Legs} />
-						<_D.Spaceship.Thruster {...DecorationConfigs.Spaceship.Thruster} />
-						<_D.Spaceship.Thruster {...DecorationConfigs.Spaceship.Thruster} />
-					</>
+					<SpaceShipMain>
+						<SpaceShipTravelContainer animate={_MotionVariants().Spaceship.vehicle.travel}>
+							<ExhaustVibrateContainer //@ts-ignore works but compiler doesnt like 'repeatType' motion property
+								animate={_MotionVariants().Spaceship.exhaust}
+							>
+								<SiCloudways style={createReactIconStyles("spaceship")} />
+							</ExhaustVibrateContainer>
+							<SpaceShipVibrateContainer
+								//@ts-ignore works but compiler doesnt like 'repeatType' motion property
+								animate={_MotionVariants().Spaceship.vehicle.vibration}
+							>
+								<FaSpaceShuttle style={createReactIconStyles("spaceship")} />
+							</SpaceShipVibrateContainer>
+						</SpaceShipTravelContainer>
+					</SpaceShipMain>
+				);
+			case "ReactIcon":
+				return <RiReactjsFill style={createReactIconStyles("default")} />;
+			case "StyledComponentIcon":
+				return <SiStyledcomponents style={createReactIconStyles("default")} />;
+			case "FramerMotionIcon":
+				<CgFramer style={createReactIconStyles("default")} />;
+			case "ReactRouterReduxIcons":
+				return (
+					<IconContainer>
+						<SiReactrouter style={createReactIconStyles("multiple", 2)} />
+						<SiRedux style={createReactIconStyles("multiple", 2)} />
+					</IconContainer>
+				);
+			case "ViteTsGithubIcons":
+				return (
+					<IconContainer>
+						<SiVite style={createReactIconStyles("multiple", 3)} />
+						<SiTypescript style={createReactIconStyles("multiple", 3)} />
+						<SiGithub style={createReactIconStyles("multiple", 3)} />
+					</IconContainer>
 				);
 			default:
-				break;
+				return <></>;
 		}
 	};
 
 	console.log("Decoration rerendered!");
-	return <_D.Main {...DecorationConfigs.Main}>{createDecoration()}</_D.Main>;
+	return <>{createVariant(variant)}</>;
 };
 
 //STYLE
-const _DVariants: _DVariants = {
-	default: {},
-	Main: {
-		default: ``,
-		Spaceship: `
-      // background: white;
-      height: 30rem;
-      width: 100%;
-      display: grid;
-      grid-template-columns: repeat(5, 20%);
-      grid-template-rows: repeat(3, 1fr);
-      grid-template-areas: "solarcontainer bodyupper bodyupper bodyupper ." ". . . . ." ". . . . .";
-    `,
-	},
-	Spaceship: {
-		SolarContainer: `
-      background: blue;
-      height: 5rem;
-      width: 10rem;
-      grid-area: solarcontainer;
-      display: flex;
-      margin-left: 50%;
-      margin-top: 40%;
-      // gap: 0.1rem;
-      // padding: 0.1rem;
-      // justify-self: start;
-      z-index: 2;
-    `,
-		BodyUpper: `
-      background-image: linear-gradient(to top, rgba(223, 189, 0, 0.7), rgba(181, 153, 0, 0.7));
-      position: relative;
-      height: 7rem;
-      width: 20rem;
-      grid-area: bodyupper;
-      justify-self: center;
-      align-self: end;
-      clip-path: polygon(0% 100%, 17.5% 0%, 82.5% 0%, 100% 100%);
-    `,
-		BodyLower: `
-    `,
-		Legs: ``,
-		Thruster: ``,
-	},
-	Details: {
+const IconContainer = styled(motion.div)`
+	display: flex;
+	width: 100%;
+	height: fit-content;
+	justify-content: center;
+	align-items: center;
+	column-gap: 4rem;
+`;
+
+const ExhaustVibrateContainer = styled(motion.div)`
+	height: 100%;
+	width: fit-content;
+`;
+
+const SpaceShipVibrateContainer = styled(motion.div)`
+	height: 100%;
+	width: fit-content;
+`;
+
+const SpaceShipTravelContainer = styled(motion.div)`
+	height: 10rem;
+	width: 10rem;
+	display: flex;
+	column-gap: 0.3rem;
+`;
+
+const SpaceShipMain = styled(motion.div)`
+	min-height: 15rem;
+	width: 100%;
+	display: flex;
+`;
+
+//only input a variant if there is a <variant>Sequence function created for it
+const _MotionVariants = () => {
+	return {
 		Spaceship: {
-			SolarPanels: ``,
+			vehicle: {
+				vibration: {
+					transform: ["translateY(0.5rem)", "translateY(0rem)", "translateY(-0.5rem)"],
+					transition: {
+						duration: 0.2,
+						repeat: Infinity,
+						repeatType: "reverse",
+					},
+				},
+				travel: {
+					opacity: [0, 0.9, 1, 1, 0.9, 0],
+					transform: [
+						"translateX(-50vw)",
+						"translateX(50vw)",
+						"translateX(100vw)",
+						"translateX(150vw)",
+					],
+					transition: {
+						duration: 5,
+						repeat: Infinity,
+						ease: "linear",
+					},
+				},
+			},
+			exhaust: {
+				transform: ["translateY(0.5rem)", "translateY(0rem)", "translateY(-0.5rem)"],
+				transition: {
+					duration: 0.1,
+					repeat: Infinity,
+					repeatType: "reverse",
+				},
+			},
 		},
-	},
+	};
 };
 
-const _D = {
-	Main: styled.div<_DProps>`
-		${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-	`,
-	Details: styled.div<_DProps>`
-		${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-	`,
-	Spaceship: {
-		SolarContainer: styled.div<_DProps>`
-			${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-		`,
-		Body: styled.div<_DProps>`
-			${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-		`,
-		Legs: styled.div<_DProps>`
-			${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-		`,
-		Thruster: styled.div<_DProps>`
-			${(p) => _DVariants[p.variant as keyof _DVariants][p.subComp as keyof _DVariants]}
-		`,
-	},
-};
-
-export default Decorations;
+export default React.memo(Decorations);
