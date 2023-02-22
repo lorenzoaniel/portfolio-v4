1e564dd738963e872b4d4122e0642da325a590f5
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-21 21:38:55 -0700

    Create ProjectThumbnail.tsx

---
+++ src/components/Image/Styles/ProjectThumbnail.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const Main = styled(motion.div)`
+	width: 100%;
+	height: 100%;
+`;
+
+export const Img = styled(motion.img)`
+	width: inherit;
+	height: inherit;
+	opacity: 0.5;
+`;
